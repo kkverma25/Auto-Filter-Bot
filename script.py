0eeab331330e8af487334cf54789839a7d89dda8
Mr.vermaji <85053195+kkverma25@users.noreply.github.com>
2022-05-11 15:40:49 +0530

    Update script.py

--- script.py
+++ script.py
@@ -5,7 +5,7 @@ class script(object):
 
 You can call this as an Auto Filter Bot if you like :D
 
-This is Version 2 of Auto Filter Bot
+This is Version of Auto Filter Bot [developer @D_E_V_l_L]
 
 Bot gives button link to files in connected channels on query !
 
@@ -53,17 +53,17 @@ No need add each filter again!
 Bot will automatically search for your files and give links to that!
 
 
-<b>© @TroJanzHEX</b>"""
+<b>© @About_EVERETT</b>"""
 
 
     ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V2</b>
     
-⭕️<b>Creater :</b> @TroJanzHEX
+⭕️<b>Creater :</b> @About_EVERETT
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 
-⭕️<b>Tutorial Video :</b> <a href='https://youtu.be/KQVYQAOsFYY'>Video Link</a> 
+⭕️<b>Movie channel:</b>  @backup_channel_000
 
 """
